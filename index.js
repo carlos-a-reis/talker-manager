0b4465e9bfed9b3db82138ab2622de2b593848a4
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-08-04 17:20:20 -0300

    Validação do email e senha do endpoint POST '/login' (requisito 04)

--- index.js
+++ index.js
@@ -26,7 +26,21 @@ app.get('/talker/:id', (req, res) => {
   res.status(200).json(talker);
 });
 
-app.post('/login', (_req, res) => {
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const validator = /\S+@\S+\.\S+/;
+  console.log(validator.test(email));
+
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!validator.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  
   res.status(200).json({ token: tokenGenerator() });
 });
 
